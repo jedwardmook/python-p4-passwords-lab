d346a0d07aeb40534fbe5efce025d70ea67ed556
JedwardMook <jedwardmook@yahoo.com>
2024-08-17 21:38:54 -0500

    tests passing

--- server/app.py
+++ server/app.py
@@ -28,16 +28,38 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if not session['user_id']:
+            return {}, 204
+        
+        user = User.query.filter(User.id == session['user_id']).first()
+        return user.to_dict(), 200
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        user = User.query.filter(User.username == username).first()
+        
+        password = data.get('password')
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 401
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
